5c8b4f27dbe1a3b0e64e2e59646be11d8d4eb2fb
Avilinguist <53711091+Avilinguist@users.noreply.github.com>
2022-01-04 15:46:41 +0800

    Update NLP_topic_model_code.py

--- NLP_topic_model_code.py
+++ NLP_topic_model_code.py
@@ -18,6 +18,32 @@ texts = [[word for word in jieba.cut(document) if word not in stopwords] for doc
 dictionary = corpora.Dictionary(texts)
 corpus = [dictionary.doc2bow(text) for text in texts]
 
+#查看对应一个topic数量，其对应的平均组间距离是什么样的
+def topic_choosing(number):
+    lda_temp = LdaModel(bow_corpus,num_topics=number,id2word=word_dictionary)
+    
+    temp_dic = {}
+    for topic_number in range(len(lda_temp.print_topics())):
+        temp_dic.setdefault(topic_number,{})
+        for term, frequency in lda_temp.show_topic(topic_number, topn=25):
+            temp_dic[topic_number].setdefault(term)
+            temp_dic[topic_number][term]=frequency #计算每个分类里前25的词的词频
+    X = pd.DataFrame(temp_dic).fillna(0)
+    
+    pairs = [record for record in combinations(X.columns,2)]
+    distance = []
+    for pair in pairs:
+        x1 = np.asarray(X[pair[0]]).reshape(-1,1)
+        x2 = np.asarray(X[pair[1]]).reshape(-1,1)
+        distance.append(cosine_distances(x1,x2))#计算不同分类间在词频上的距离
+    average_distance = np.average(distance)#计算平均距离
+    
+    return average_distance
+
+#筛选类别
+distance = [topic_choosing(i) for i in range(4,11,1)]
+sn.lineplot(range(4,11,1),distance) #7是个好类别
+
 #LDA Topic model
 lda = LdaModel(corpus,num_topics=15,id2word=dictionary) #topic的数量可以自己指定；这一步非常算力consuming
 
